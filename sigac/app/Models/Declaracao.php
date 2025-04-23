f4d7c3314ad1e05eb60d5dcddc2d96a650158c43
mitugui <cristian.mitugui04@gmail.com>
2025-04-22 21:26:20 -0300

    chore: migração do código do projeto antigo

---
+++ sigac/app/Models/Declaracao.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Declaracao extends Model
+{
+    use HasFactory;
+
+    protected $table = 'declaracoes';
+    protected $fillable = ['hash', 'data'];
+}
